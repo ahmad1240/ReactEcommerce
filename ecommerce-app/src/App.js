d7a84edd0add73c9994958aac3b552e88233192d
Mohammad Ahmad <mohammadahmad4@gmail.com>
2024-01-29 19:37:11 +0530

    ecommerce work

---
+++ ecommerce-app/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/header/header.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
