37916b789973bcaf3873c95bf57e8a1a7b10789d
Mohammad Ahmad <mohammadahmad4@gmail.com>
2024-01-29 20:09:49 +0530

    start

--- ecommerce-app/src/components/header/header.js
+++ ecommerce-app/src/components/header/header.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "../header/header.css";
+import Logo from "../../assets/images/logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2">
+              <img src={Logo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
